0c2e3293e0449015373d94de2f2904135891b0bc
CARDinal <93416662+CARDinalCAWWW@users.noreply.github.com>
2026-02-03 12:52:18 -0500

    Projectile

--- player.gd
+++ player.gd
@@ -4,10 +4,10 @@ var mouse_sensitivity := 0.002
 var twist_input := 0.0
 var pitch_input := 0.0
 
-var move_force := 1100.0
-var jump_force := 8
+var move_force := 25.0
+var jump_force := 8.0
 
-var coyote_time := .2
+var coyote_time := 0.2
 var coyote_timer := 0.0
 
 var jumps_remaining := 0
@@ -20,45 +20,75 @@ var max_jumps := 2
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta: float) -> void:
+	# -------------------------
+	# MOVEMENT
+	# -------------------------
 	var input := Vector3.ZERO
-	
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
-	apply_central_force(twist_pivot.basis * input * move_force * delta)
-	
+
+	# Apply movement force (no delta)
+	apply_central_force(twist_pivot.basis * input * move_force)
+
+	# -------------------------
+	# COYOTE TIME + JUMP RESET
+	# -------------------------
 	if ground_ray.is_colliding():
 		coyote_timer = coyote_time
 		jumps_remaining = max_jumps
 	else:
 		coyote_timer -= delta
-	
-	if Input.is_action_just_pressed("move_jump") and coyote_timer > 0.0:
-		apply_central_impulse(Vector3.UP * jump_force)
-		coyote_timer = 0.0
-		jumps_remaining -= 1
-	elif Input.is_action_just_pressed("move_jump") and jumps_remaining > 1:
-		apply_central_impulse(Vector3.UP * jump_force)
-		jumps_remaining -= 1
-		
+
+	if Input.is_action_just_pressed("move_jump"):
+		if coyote_timer > 0.0:
+			apply_central_impulse(Vector3.UP * jump_force)
+			coyote_timer = 0.0
+			jumps_remaining -= 1
+		elif jumps_remaining > 1:
+			apply_central_impulse(Vector3.UP * jump_force)
+			jumps_remaining -= 1
+
 	if Input.is_action_just_pressed("attack"):
 		$TwistPivot/PitchPivot/Bat.swing()
-		
+
+	# -------------------------
+	# THROWING
+	# -------------------------
 	if Input.is_action_just_pressed("throw"):
-		$TwistPivot/PitchPivot/Bat.throw()
-		
+		var projectile := preload("res://Weapong_throw.tscn").instantiate()
+		get_tree().current_scene.add_child(projectile)
+
+		# Spawn at camera/bat position
+		projectile.global_transform = $TwistPivot/PitchPivot.global_transform
+
+		# Throw forward
+		var forward: Vector3 = -$TwistPivot/PitchPivot.global_transform.basis.z
+		projectile.apply_central_impulse(forward * 20.0)
+
+	# -------------------------
+	# MOUSE RELEASE
+	# -------------------------
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-	twist_pivot.rotate_y(twist_input)
-	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp( pitch_pivot.rotation.x, deg_to_rad(-30), deg_to_rad(30))
+
+	# -------------------------
+	# CAMERA ROTATION
+	# -------------------------
+	twist_pivot.rotation.y += twist_input
+	pitch_pivot.rotation.x = clamp(
+		pitch_pivot.rotation.x + pitch_input,
+		deg_to_rad(-30),
+		deg_to_rad(30)
+	)
+
 	twist_input = 0.0
 	pitch_input = 0.0
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
+			twist_input = -event.relative.x * mouse_sensitivity
+			pitch_input = -event.relative.y * mouse_sensitivity
--- weapon_bat.gd
+++ weapon_bat.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 var swinging: bool = false
-var throwing: bool = false
 
 var original_rot: Vector3
 var original_parent: Node = null
@@ -19,7 +18,7 @@ func _ready():
 # SWING ATTACK
 # -------------------------
 func swing():
-	if swinging or throwing:
+	if swinging:
 		return
 	swinging = true
 
@@ -28,36 +27,6 @@ func swing():
 	rotation_degrees = original_rot
 
 	swinging = false
-
-# -------------------------
-# THROW ATTACK
-# -------------------------
-func throw():
-	if throwing or swinging:
-		return
-	throwing = true
-
-	# Save original parent + transform
-	original_parent = get_parent()
-	original_transform = global_transform
-
-	# Reparent to world so it no longer follows the camera
-	get_tree().current_scene.add_child(self)
-	global_transform = original_transform
-
-	# Disable hitbox briefly so it doesn't hit the player
-	$Area3D.monitoring = false
-	await get_tree().create_timer(0.1).timeout
-	$Area3D.monitoring = true
-
-	set_physics_process(true)
-
-func _physics_process(delta):
-	if throwing:
-		# Move forward
-		global_position += -global_transform.basis.y * throw_speed * delta
-		rotation_degrees.x = original_rot.x + 90
-
 # -------------------------
 # HIT DETECTION
 # -------------------------
@@ -65,19 +34,3 @@ func _on_hit(body):
 	if swinging and body.is_in_group("enemy"):
 		body.queue_free()
 		return
-
-	if throwing:
-		if body.is_in_group("enemy"):
-			body.queue_free()
-
-		_stop_throw()
-
-# -------------------------
-# STOP THROW (NO RETURN)
-# -------------------------
-func _stop_throw():
-	throwing = false
-	set_physics_process(false)
-
-	# Weapon stays where it landed
-	# No reparenting, no snapping back
