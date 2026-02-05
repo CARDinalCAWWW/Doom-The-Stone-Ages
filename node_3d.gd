d9c0a6c389dd965d00638677c29988ce60c7e704
JIM-school1 <gingy305@gmail.com>
2026-02-05 12:17:40 -0500

    weapon switch

--- node_3d.gd
+++ node_3d.gd
@@ -1,11 +1,6 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Sprite3D/SubViewport/ProgressBar.value -= 1
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		var bar := $Sprite3D/SubViewport/ProgressBar
+		bar.value = max(bar.value - 2, bar.min_value)
