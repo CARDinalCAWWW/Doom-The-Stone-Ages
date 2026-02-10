30aeee1975a24bbfd9871db14253aa1560cb0d93
JIM-school1 <gingy305@gmail.com>
2026-02-10 13:01:27 -0500

    Ammo

--- level.gd
+++ level.gd
@@ -1,6 +1,22 @@
-extends Node3D
+extends Node
 
-@onready var player = $RigidBody3D
+var max_ammo := 20
+var ammo := max_ammo
 
-func  _physics_process(delta: float) -> void:
-	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
+func _ready() -> void:
+	update_ammo_ui()
+
+func _process(delta: float) -> void:
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		shoot()
+
+func shoot() -> void:
+	if ammo > 0:
+		ammo -= 1
+		update_ammo_ui()
+	else:
+		print("No ammo left!")
+
+func update_ammo_ui() -> void:
+	var label := $CanvasLayer/AmmoLabel
+	label.text = "Ammo: %d / %d" % [ammo, max_ammo]
